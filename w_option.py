844def90e7b6a21c0c8083ae1be94fffb66b94ba
Javohir <hp15032004@gmail.com>
2025-01-22 19:28:30 +0500

    news

--- w_option.py
+++ w_option.py
@@ -1,5 +1,12 @@
 
 
-with open("example.txt", "w") as file:
-    file.write("bu yangi fayl.\n")
-    file.write("ikkinchi qator.\n")
+# with open("example.txt", "w") as file:
+#     file.write("bu yangi fayl.\n")
+#     file.write("ikkinchi qator.\n")
+
+
+
+
+with open("example.txt", "a") as file:
+    file.write("bu qacongi.\n")
+    file.write("2-qator.\n")
