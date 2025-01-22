f73b38175aba5bafdd3d127b42a9780c69617c96
Javohir <hp15032004@gmail.com>
2025-01-22 19:20:56 +0500

    news

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-wwqwqf
-ewq
-defwqfe
+
+
+file = open("example.txt", "r")
+content = file.read()
+print(content)
+file.close()
+
+with open("example.txt", "r") as file:
+    content = file.read()
+    print(content)
+
