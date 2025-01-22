f129fb5f51d6209401b8bced0a9332108419546f
Javohir <hp15032004@gmail.com>
2025-01-22 20:01:03 +0500

    news

--- os_module.py
+++ os_module.py
@@ -1,11 +1,21 @@
 
+#
+# import os
+#
+# file_path = "example.txt"
+# if os.path.exists(file_path):
+#     os.remove(file_path)
+#     print(f"fayl {file_path} ochirildi")
+#
+# else:
+#     print("fayl mavjud emas")
+
 
 import os
 
-file_path = "example.txt"
-if os.path.exists(file_path):
-    os.remove(file_path)
-    print(f"fayl {file_path} ochirildi")
-
+dir_path = "manzil"
+if os.path.exists(dir_path) and os.path.isdir(dir_path):
+    os.rmdir(dir_path)  ## bosh direktoriyani ochiradi
+    print(f"Direktoriya {dir_path} ochirildi. ")
 else:
-    print("fayl mavjud emas")
+    print("Direktoriyaning mavjud emasligi yoki bosh emasligi. ")
